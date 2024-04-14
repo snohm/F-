01b1e577fb3a6880e20e894f94f9bc3ac28852b9
snohm <alessandro.beneggi@gmail.com>
2024-04-14 19:13:13 +0200

    .

--- lez09-moduli/exISET.fsx
+++ lez09-moduli/exISET.fsx
@@ -1,4 +1,4 @@
-Aggiungere all'interfaccia iset.fsi le seguenti funzioni
+(*Aggiungere all'interfaccia iset.fsi le seguenti funzioni
 
 count : IFSet -> int, che computa la cardinalità dell'insieme
 
@@ -18,3 +18,4 @@ min : IFSet-> int, che calcola il minimo elelmento di un insieme non vuoto
 --> Implementare le dette funzioni sia come liste che come alberi.
 
 --> Estendere i test fscheck dati a coprire le nuove funzionalità
+*)
